3ef7c01ceafe5cc6796ee1a347e8892f490c0b86
Wavemaker Cards Novel Writing Software <cards@wavemaker.co.uk>
2025-12-10 21:15:11 +0000

    jumped to 4.1.14  adn added button style to tags on cards DB

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c30nrc103d"
+    "revision": "0.h4hc02i0c48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
